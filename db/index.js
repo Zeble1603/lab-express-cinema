e6381dd0865d449ac3dacaf56fd2a8296f614724
Blaise <blaisedecarvalho@gmail.com>
2022-06-26 19:58:03 +0200

    lab done

--- db/index.js
+++ db/index.js
@@ -1,3 +1,6 @@
+//Import our data
+const movies = require('../seeds/movies.seed');
+
 // ℹ️ package responsible to make the connection with mongodb
 // https://www.npmjs.com/package/mongoose
 const mongoose = require("mongoose");
@@ -5,12 +8,25 @@ const mongoose = require("mongoose");
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
+//Import and use our model
+const Movie = require('../models/movie.model');
+
 const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-cinema";
 
 mongoose
   .connect(MONGO_URI)
   .then((x) => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+    return Movie.deleteMany()
+  })
+  .then((response)=>{
+    console.log(`Database cleaned and ready to go`);
+    return Movie.insertMany(movies)
+  })
+  .then((response)=>{
+    for(film of response){
+      console.log(`${film.title} added succesfuly`);
+    }
   })
   .catch((err) => {
     console.error("Error connecting to mongo: ", err);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,30 @@
+const { response } = require('express');
 const express = require('express');
 const router = express.Router();
+const Movies = require('../models/movie.model');
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req,res,next) =>{
+    Movies.find()
+    .then((movieArray)=>{
+        res.render('movies.hbs', {movieArray})
+    })
+    .catch((err)=>{
+        next(err)
+    })
+})
+
+router.get('/movie/:id', (req,res,next) =>{
+    Movies.findById(req.params.id)
+    .then((movieDetail)=>{
+        console.log("TEST MOVIE DETAIL", movieDetail)
+        res.render('movieDetail.hbs', {movieDetail})
+    })
+    .catch((err)=>{
+        next(err)
+    })
+})
 
 module.exports = router;
